355e347531b05431ce0213174a1f84aa66d3186b
unknown <hitprofit70@gmail.com>
2023-05-11 12:01:55 +0200

    image changed

--- src/APIData/PantsData.js
+++ src/APIData/PantsData.js
@@ -1,28 +1,28 @@
 const pants = [
     {   
         id: 5,
-        name: 'New Balance Kick Game',
+        name: 'Grid Print Slant',
         price: '299.95',
         pImg: 'https://img.ltwebstatic.com/images3_pi/2022/05/05/1651720300fa09f2e118701f9067c944d8b596845d_thumbnail_900x.webp',
         rate: 3,
     },
     {   
         id: 6,
-        name: 'New Balance Kick Game',
+        name: 'Striped Drawstring Waist',
         price: '199.95',
         pImg: 'https://img.ltwebstatic.com/images3_pi/2021/11/16/16370465406591715d5da4be59079d8097260ccc48_thumbnail_900x.webp',
         rate: 3,
     },
     {   
         id: 7,
-        name: 'New Balance Kick Game',
+        name: 'Striped Print',
         price: '159.95',
         pImg: 'https://img.ltwebstatic.com/images3_pi/2022/01/07/164152255743d56d33677149519095fdbe4987ba3d_thumbnail_900x.webp',
         rate: 3,
     },
     {   
         id: 8,
-        name: 'New Balance Kick Game',
+        name: 'Drawstring Waist',
         price: '499.95',
         pImg: 'https://img.ltwebstatic.com/images3_pi/2023/04/27/1682585523dbbe2249107c16271145b3de42cfc057_thumbnail_900x.webp',
         rate: 3,
