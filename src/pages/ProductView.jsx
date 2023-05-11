8450e521d1683df41c1dca981d20fa9a2af39e47
unknown <hitprofit70@gmail.com>
2023-05-11 15:44:19 +0200

    useeffect added

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import ProductsData from "../APIData/ProductsData";
+import PantsData from "../APIData/PantsData"
 import TopNav from "../components/TopNav";
 import Floor from "../components/Floor";
 import Stack from "react-bootstrap/Stack";
@@ -10,9 +11,15 @@ import Col from "react-bootstrap/Col";
 const ProductView = () => {
   const { id } = useParams();
   const [products, setProducts] = useState(null);
+  const [pants, setPants] = useState(null);
   useEffect(() => {
     setProducts(() => {
       return ProductsData.find((p) => p.id === Number(id));
+    });
+  }, [id]);
+  useEffect(() => {
+    setPants(() => {
+      return PantsData.find((p) => p.id === Number(id));
     });
   }, [id]);
 
@@ -36,6 +43,37 @@ const ProductView = () => {
                 <Stack className="text-center mr-5 mt-3 mb-3" gap={3}>
                   <h4 className="bg-dark mr-5 text-light">{products.name}</h4>
                   <h6 className="border">R{products.price}</h6>
+                  <span className="border">Free Delivery</span>
+                  <div className="">
+                    {" "}
+                    <button
+                      className="add-button mt-3"
+                      onClick={() => addtoCart(products)}
+                    >
+                      AddtoCart
+                    </button>
+                  </div>
+                </Stack>
+              </Col>
+            </Row>
+          </div>
+        )}
+        {pants && (
+          <div className="row">
+            <Row>
+              <Col
+                style={{ width: "350px", height: "350px" }}
+              >
+                <img className="mt-4"
+                  src={pants.pImg}
+                  alt=""
+                  style={{ width: "300px", height: "300px", marginRight: "auto", marginLeft: "auto" }}
+                />
+              </Col>
+              <Col style={{ width: "350px", height: "350px" }}>
+                <Stack className="text-center mr-5 mt-3 mb-3" gap={3}>
+                  <h4 className="bg-dark mr-5 text-light">{pants.name}</h4>
+                  <h6 className="border">R{pants.price}</h6>
                   <span className="border">Free Delivery</span>
                   <div className="">
                     {" "}
