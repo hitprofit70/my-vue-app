8cffe6ec6c04600dbc39a1797acad42ba9ff224e
unknown <hitprofit70@gmail.com>
2023-05-16 11:51:59 +0200

    icon added

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -4,24 +4,99 @@ import Col from "react-bootstrap/Col";
 const CartSummary = () => {
   return (
     <div className="container mt-5">
-      <Col className="mb-5 shadow bg-body rounded"
+      <Col
+        className="mb-4 shadow bg-body rounded"
         style={{
           width: "350px",
-          height: "300px",
+          height: "230px",
           marginLeft: "auto",
-          border: "solid 1px",
         }}
       >
         <Stack className="text-center mt-3" gap={3}>
-          <h4 className="bg-dark text-light" style={{width: "250px", marginLeft: "auto", marginRight: "auto"}}>Cart Summary</h4>
-          <h6 className="border" style={{width: "250px", marginLeft: "auto", marginRight: "auto"}}>R23456.09</h6>
-          <span className="border" style={{width: "250px", marginLeft: "auto", marginRight: "auto"}}>Free Delivery</span>
+          <h4
+            className="bg-dark text-light"
+            style={{ width: "250px", marginLeft: "auto", marginRight: "auto" }}
+          >
+            Cart Summary
+          </h4>
+          <h6
+            className="border"
+            style={{ width: "250px", marginLeft: "auto", marginRight: "auto" }}
+          >
+            R23456.09
+          </h6>
+          <span
+            className="border"
+            style={{ width: "250px", marginLeft: "auto", marginRight: "auto" }}
+          >
+            Free Delivery
+          </span>
           <div className="">
             {" "}
             <button className="checkout-button mt-3">
-              Proccesed to Checkout
+              Proceed to Checkout
             </button>
           </div>
+        </Stack>
+      </Col>
+      <Col
+        className="mb-5 shadow bg-body rounded"
+        style={{
+          width: "350px",
+          height: "150px",
+          marginLeft: "auto",
+        }}
+      >
+        <Stack className="text-center mt-3" gap={3}>
+          <span
+            className="text-center"
+            style={{ width: "250px", marginLeft: "auto", marginRight: "auto" }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-lock-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
+            </svg>
+
+            Secure checkout
+          </span>
+          <span
+            className="text-center"
+            style={{ width: "250px", marginLeft: "auto", marginRight: "auto" }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-credit-card-2-back-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5H0V4zm11.5 1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-2zM0 11v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1H0z" />
+            </svg>
+            Many ways to pay
+          </span>
+          <span
+            className="center"
+            style={{ width: "250px", marginLeft: "auto", marginRight: "auto" }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-truck"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+            </svg>
+            Fast, reliable delivery
+          </span>
         </Stack>
       </Col>
     </div>
--- src/components/ShoppingCard.jsx
+++ src/components/ShoppingCard.jsx
@@ -1,18 +1,29 @@
+import { useContext } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import AppContext from "../context/AppContext";
 
 const ShoppingCard = () => {
+  const { cart } = useContext(AppContext);
   return (
     <div className="container mt-5">
-      <Card className="text-center mt-5 shadow bg-body rounded" style={{height: "15rem"}}>
-        <Card.Body>
-          <Card.Title>Logo</Card.Title>
-          <Card.Text className="mt-5">
-          Your shopping cart is empty
-          </Card.Text>
-          <Button variant="dark" className="mt-5">Contuine Shopping</Button>
-        </Card.Body>
-      </Card>
+      {cart.length === 0 && (
+        <Card
+          className="text-center mt-5 shadow bg-body rounded"
+          style={{ height: "15rem" }}
+        >
+          <Card.Body>
+            <Card.Title>Logo</Card.Title>
+            <Card.Text className="mt-5">Your shopping cart is empty</Card.Text>
+            <Link to="/">
+              <Button variant="dark" className="mt-5">
+                Contuine Shopping
+              </Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      )}
     </div>
   );
 };
