12b3f53be5fda530d98228dcfee86fbbe1e8eb36
unknown <hitprofit70@gmail.com>
2023-05-23 14:10:32 +0200

    register form style added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const sendLogin = () => {};
 
   const register = () => {
-    navigator("/login");
+    navigator("/register");
   };
 
   return (
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
 import TopNav from "../components/TopNav";
 import Floor from "../components/Floor";
+import toast from 'react-hot-toast';
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [contactNo, setContactNo] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const sendRegister = () => {
+    if (!firstName || !lastName || !contactNo || !email || !password || !confirmPassword) {
+      toast.error("All the input are required.");
+      return;
+    }
+  }
+
   return (
     <div>
       <TopNav />
       <div className="container mt-5 mb-5">
-        <h1>Hello Register</h1>
+      <div
+          className="login-form shadow bg-body rounded"
+          style={{
+            width: "450px",
+            height: "550px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            borderRadius: "20px",
+          }}
+        >
+          <div className="text-center">
+           <h2 className="">Register</h2>
+          </div>
+          <div className="mt-5">
+          <input
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+                <input
+              type="text"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+                <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Contact Number"
+              value={contactNo}
+              onChange={(e) => setContactNo(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+                <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <button className="login-button mt-5" onClick={sendRegister}>
+            Register
+          </button>
+        </div>
       </div>
       <Floor />
     </div>
