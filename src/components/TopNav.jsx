09b8ada6b3cb8ed9d78f440ed6db60669efa18e7
unknown <hitprofit70@gmail.com>
2023-05-24 14:57:53 +0200

    navbar fixed

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,8 +1,5 @@
 import { useContext } from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import AppContext from "../context/AppContext";
 
@@ -11,35 +8,54 @@ const TopNav = () => {
 
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>My Shopping</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link>
-              <Link to="/">Home</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/sneakers">Sneakers</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/pants">Pants</Link>
-            </Nav.Link>
-            <Nav.Link className="cart" style={{ marginLeft: "700px" }}>
-              <Link to="/cart">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="white"
-                  className="bi bi-bag"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                </svg>
-                <span className="count">{cart.length}</span>
-              </Link>
-            </Nav.Link>
-            <NavDropdown title="My Account" id="basic-nav-dropdown">
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container">
+          <a className="navbar-brand text-white">My Shopping</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/">
+                  <a className="nav-link active text-white">Home</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/sneakers">
+                  <a className="nav-link active text-white">Sneakers</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/pants">
+                  <a className="nav-link active text-white">Pants</a>
+                </Link>
+              </li>
+              <li className="nav-item" style={{ marginLeft: "800px" }}>
+                <Link to="/cart">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="white"
+                    className="bi bi-bag"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                  </svg>
+                  <span className="count">{cart.length}</span>
+                </Link>
+              </li>
+              <li className="nav-item dropdown text-white">
+              <NavDropdown className="text-white" title="My Account" id="basic-nav-dropdown text-white">
               <Link to="/login">
               <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
               </Link>
@@ -47,9 +63,37 @@ const TopNav = () => {
               <NavDropdown.Item href="#action/3.2">Register</NavDropdown.Item>
               </Link>
             </NavDropdown>
-          </Nav>
-        </Container>
-      </Navbar>
+                {/* <a
+                  className="nav-link dropdown-toggle text-white"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  My Account
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul> */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
